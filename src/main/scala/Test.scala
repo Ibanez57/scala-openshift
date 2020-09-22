b8b802ab98a1cfa74519c791476270e07ef5cfe5
Student User <student@workstation.lab.example.com>
2020-09-22 08:24:00 -0400

    Akka Cluster

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -1,8 +1,13 @@
+import akka.actor.ActorSystem
+
+
 object Test extends App {
 	println("========")
 	println("= Test =")
 	println("========")
 
+	implicit val system: ActorSystem = ActorSystem("Test")
+
 	// To prevent Openshift from restarting the pod indefinitely
-	while (true) Thread.sleep(10000)
+	//while (true) Thread.sleep(10000)
 }
