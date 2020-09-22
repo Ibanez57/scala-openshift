e39136d7a564f79135a11f886dd6b39bd4778f09
Student User <student@workstation.lab.example.com>
2020-09-22 05:49:22 -0400

    Custom assemble and run

--- src/main/scala/Test.scala
+++ src/main/scala/Test.scala
@@ -2,4 +2,7 @@ object Test extends App {
 	println("========")
 	println("= Test =")
 	println("========")
+
+	// To prevent Openshift from restarting the pod indefinitely
+	while (true) Thread.sleep(10000)
 }
